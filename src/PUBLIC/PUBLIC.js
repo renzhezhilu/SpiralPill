099fe57886fe92eed43e4732d4327e11db7f1b20
silaix2 <mac@macdeMacBook-Air.local>
2018-09-24 03:42:15 +0800

    新建表格功能
    代码还能再优化

---
+++ src/PUBLIC/PUBLIC.js
@@ -0,0 +1,6 @@
+//公用
+document.writeln("<link rel=\"stylesheet\" type=\"text/css\" href=\"../PUBLIC/css/reset.css\">");
+document.writeln("<link rel=\"stylesheet\" type=\"text/css\" href=\"../PUBLIC/css/pure-min.css\">");
+document.writeln("<link rel=\"stylesheet\" type=\"text/css\" href=\"../PUBLIC/css/font-awesome.min.css\">");
+document.writeln("<script type=\"text/javascript\" src=\"../PUBLIC/js/less.min.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"../PUBLIC/js/jquery.min.js\"></script>");
